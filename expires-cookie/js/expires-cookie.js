3ce96c77661382eb01505858e7afb65aeb5fbd46
nasangw <nasangw@29cm.co.kr>
2016-11-30 14:38:21 +0900

    Modify - complete expires-cookie.js and sample page(version 0.1)

--- expires-cookie/js/expires-cookie.js
+++ expires-cookie/js/expires-cookie.js
@@ -6,31 +6,36 @@
 
 		var defaults = {
 			target: undefined,
-			duration: '1d',
-			def1: true,
-			def2: false,
-			def3: null,
-			def4: undefined,
-			def5: 52,
-			def6: "text",
-			def7: ["arr1", "arr2"]
+			duration: '1d'
 		},
 		e = this;
 
 		if(elem !== null) {
-			// Define Element of ExpiresCookie
-			e.elem = typeof elem === "string" ? document.querySelectorAll(elem) : elem;
+			// When define element
+
+			/* e.elem = typeof elem === "string" ? document.querySelectorAll(elem) : elem;
 
 			if (e.elem.length > 1) {
 				var expiresArray = [];
-				e.elem.forEach(function(element) {
-					expiresArray.push(new ExpiresCookie(element, params));
+				e.elem.forEach(function(el) {
+					expiresArray.push(new ExpiresCookie(el, params));
 				});
 
 				return expiresArray;
 			}
+			e.elem = e.elem.length !== undefined ? e.elem : e.elem[0]; */
 
-			e.elem = e.elem.length === undefined ? e.elem : e.elem[0];
+			// Define Element of ExpiresCookie
+			e.elem = document.querySelector(elem);
+
+			// Save instance in elem HTML Element and in data
+			e.elem.expires = e;
+
+			// Set event of element
+			e.elemEvent = e.elem.nodeName === "INPUT" && (e.elem.type === ("checkbox" || "radio")) ? "change" : "click";
+		}else{
+			// When no element
+			e.elemEvent = "click";
 		}
 
 		// Assign options to params
@@ -48,47 +53,98 @@
 			}
 		}
 
-		// set duration
-		params.duration = e.elem ? (e.elem.getAttribute("data-expires") || params.duration) : params.duration;
+		if(!params.target) {
+			if(e.elem.getAttribute("data-expires-target")) {
+				params.target = e.elem.getAttribute("data-expires-target");
+			}else{
+				return;
+			}
+		}
+
+		// Set duration
+		params.duration = e.elem ? (e.elem.getAttribute("data-expires-duration") || params.duration) : params.duration;
+
+		// Set params
+		e.params = params;
+
+		// Set prototype
+		e.setExpiresDate = this.setExpiresDate;
+
+		// Set element of target
+		e.elemTarget = document.querySelectorAll(params.target);
 
 		if(elem !== null) {
-			// Save instance in elem HTML Element and in data
-			e.elem.expires = e;
+			// Bind event to element
+			e.elem.addEventListener(e.elemEvent, function(ev){
+				ev.preventDefault();
+				e.elemTarget.forEach(function(el, idx) {
+					el.style.display = "none";
+				});
+				e.setExpiresDate(params.duration);
+			}, false);
+		}else{
+			e.elemTarget.forEach(function(el, idx) {
+				el.style.display = "none";
+			});
+			e.setExpiresDate(params.duration);
 		}
 	}
 
 	// Define prototype
 	ExpiresCookie.prototype = {
-		get: function() {
-			console.log('get!!');
-		},
-
-		set: function() {
-			console.log('set!!');
-		},
-
 		setExpiresDate: function(duration) {
-			var isToday,
-				seconds = 60,
-				minutes = 60,
-				hours = 24,
-				expiresData;
+			var extractDuration,
+				attribute = {
+					duration: this.params.duration || this.elem.getAttribute("data-expires-duration"),
+					target: this.params.target || this.elem.getAttribute("data-expires-target")
+				},
+				regExp = {
+					check: /^[0-9\.]{1,10}[a-zA-Z]{1}$/,
+					isInt: /^[0-9\.]{1,}/,
+					isStr: /[a-zA-z]{1}$/
+				};
 
-			if(!duration) {
-				duration = 1;
-			}else if(duration === 'today') {
-				isToday = true;
-				expiresData = getRemainToday();
-			}else {
-				duration = parseInt(duration);
+			if(attribute.duration === "today") {
+				extractDuration = this.getRemainToday();
+			}else{
+				var date = new Date(),
+					hours = 24,
+					cookieValue = "close",
+					durationDefault = "1d",
+					checkValidateDuration = regExp.check.test(attribute.duration),
+					extractDurationInt = parseFloat(regExp.isInt.exec(attribute.duration)[0]),
+					extractDurationStr = regExp.isStr.exec(attribute.duration)[0];
+
+				if(!checkValidateDuration) {
+					return;
+				}
+
+				if(!attribute.duration) {
+					attribute.duration = durationDefault;
+				}
+
+				switch(extractDurationStr) {
+					case 'h':
+						extractDuration = (1 / hours) * extractDurationInt;
+						break;
+					case 'd':
+						extractDuration = extractDurationInt;
+						break;
+					case 'w':
+						extractDuration = extractDurationInt * 7;
+						break;
+					case 'm':
+						extractDuration = new Date(date.setMonth(date.getMonth() + extractDurationInt));
+						break;
+					case 'y':
+						extractDuration = new Date(date.setYear(date.getFullYear() + extractDurationInt));
+						break;
+				}
+				isToday = false;
 			}
 
-			if(!isToday) {
-				expiresData = duration * hours * minutes * seconds;
-			}
-
-			Cookies.set(id, 'closed', {
-				expires: expiresData
+			Cookies.set(attribute.target, cookieValue, {
+				expires: extractDuration
 			});
 		},
 
@@ -104,15 +160,28 @@
 			date.setMinutes(0);
 			date.setSeconds(0);
 			date.setDate(date.getDate() + 1);
-			tomorrow = date.getTime(); // 내일 0시 0분 으로 설정
+			tomorrow = date.getTime();
 
 			now = new Date();
-			gap = Math.round((tomorrow - now.getTime()) / 1000);
+			gap = (tomorrow - now.getTime()) / (1000 * 60 * 60 * 24);
 			return gap;
 		}
 	};
 
+	function showExpiresContents(target) {
+		if(!target || !Cookies) {
+			return;
+		}
+		if(!Cookies.get(target)) {
+			var elem = document.querySelectorAll(target);
+			elem.forEach(function(el, idx) {
+				el.style.display = "block";
+			});
+		}
+	}
+
 	window.ExpiresCookie = ExpiresCookie;
+	window.showExpiresContents = showExpiresContents;
 })();
 
 /*===========
